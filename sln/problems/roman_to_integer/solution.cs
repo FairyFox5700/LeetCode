c09121454eba0dd47dd831475fb845d52d9fceb7
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2022-06-04 19:26:15 +0000

    Sync LeetCode submission - Roman to Integer (csharp)

---
+++ sln/problems/roman_to_integer/solution.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    
+          private readonly Dictionary<char, int> dict = new Dictionary<char, int>{{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        public int RomanToInt(string s) {
+        
+        //special situation  
+        s = s.Replace("IV","IIII");
+        s = s.Replace("IX","VIIII");
+        s = s.Replace("XL","XXXX");
+        s = s.Replace("XC","LXXXX");
+        s = s.Replace("CD","CCCC");
+        s = s.Replace("CM","DCCCC");
+        
+        int result = 0;
+        
+        foreach(var ch in s)
+        {
+            result += dict[ch];
+        }
+        
+        return result;
+    }
+}
