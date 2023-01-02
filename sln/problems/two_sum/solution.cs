3f4662b9a36fe68ef19f707a2c0dda7bcaf73f38
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2023-01-02 10:59:42 +0000

    Sync LeetCode submission - Two Sum (csharp)

--- sln/problems/two_sum/solution.cs
+++ sln/problems/two_sum/solution.cs
@@ -1,23 +1,20 @@
-public class Solution {
-    public int[] TwoSum(int[] nums, int target)
-        {
-            var dictionary = new Dictionary<int, int>();
-            for (var i = 0; i < nums.Length; i++)
-            {
-                var result = target - nums[i];
-                if (dictionary.ContainsKey(result))
-                {
-                    return new[] { dictionary[result], i };
-                }
-                else
-                {
-                    if(!dictionary.ContainsKey(nums[i]))
-                    {
-                       dictionary.Add(nums[i], i); 
-                    }
-                }
-            }
 
-            return new int[] { };
+public class Solution {
+    private Dictionary<int,int> set = new Dictionary<int,int>();
+
+    public int[] TwoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.Count(); i++)
+        {
+            var diff = target - nums[i];
+            if(set.ContainsKey(diff))
+            {
+                return new int[2]{i, set[diff]};
+            }
+            if(!set.ContainsKey(nums[i]))
+            {
+               set.Add(nums[i],i);
+            }
         }
+         return new int[2] {0,0};
+    }
 }
