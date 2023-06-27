10d6983be79ab157404b180ca6302f81ed9a3bb9
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2023-06-27 18:42:43 +0000

    Sync LeetCode submission - Binary Search (csharp)

--- sln/problems/binary_search/solution.cs
+++ sln/problems/binary_search/solution.cs
@@ -1,26 +1,25 @@
 public class Solution {
- public int Search(int[] nums, int target)
+    public int Search(int[] nums, int target) {
+        var left = 0;
+        var right = nums.Count()-1;
+        while(left <= right)
         {
-
-            var left = 0;
-            var right = nums.Length-1;
-            while (left<=right)
+            var mid = (left + right) / 2;
+            Console.WriteLine(mid);
+            if( target == nums[mid])
             {
-                var mid = (left+right)/2;
-                if (target == nums[mid])
-                {
-                    return mid;
-                }
-                if (target < nums[mid])
-                {
-                    right = mid - 1;
-                }
-                else
-                {
-                    left = mid + 1;
-                }
+                return mid;
             }
-
-            return -1;
+            else if (target < nums[mid])
+            {
+                right = mid-1;
+            }
+            else
+            {
+                left = mid+1;
+            }
         }
+
+        return -1;
+    }
 }
