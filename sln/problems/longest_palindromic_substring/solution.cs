3d01004fd09a3e7e532b4dd98050da992f02df6f
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2023-08-18 17:43:00 +0000

    Sync LeetCode submission - Longest Palindromic Substring (csharp)

--- sln/problems/longest_palindromic_substring/solution.cs
+++ sln/problems/longest_palindromic_substring/solution.cs
@@ -1,30 +1,38 @@
-public class Solution {
-    public string LongestPalindrome(string s) {
-        if(s=="" || s.Length<=1)
-            return s;
-        int length = 0, start = 0;
-        for(int i=0; i<s.Length; i++)
+    public class Solution
+    {
+                public string LongestPalindrome(string s)
         {
-            int evenLength = PalindromeLength(s, i, i+1);
-            int oddLength = PalindromeLength(s, i, i);
-            int currentLength = Math.Max(evenLength, oddLength);
-            
-            if(currentLength>length)
+            //https://leetcode.com/problems/longest-palindromic-substring/solutions/3911431/python-beats-95-expanding-around-centers-approach-explained/ -- explanation
+            var maxLength = 0;
+        var maxLengthStr= new ReadOnlySpan<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int left = i; int right = i;
+            while (left >= 0 && right < s.Length && s[left] == s[right])
             {
-                length = currentLength;
-                start = i-(length-1)/2;
+                if (right - left + 1 > maxLength)
+                {
+                    maxLength = right - left + 1;
+                    maxLengthStr = s.AsSpan(left, maxLength);
+                }
+                left--;
+                right++;
             }
+            left = i; right = i+1;
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                if (right - left + 1 > maxLength)
+                {
+                    maxLength = right - left + 1;
+                    maxLengthStr = s.AsSpan(left, maxLength);
+                }
+                left--;
+                right++;
+            }
+
         }
-        
-        return s.Substring(start, length);
-    }
-    
-    private int PalindromeLength(string s, int left, int right){
-        while(left>=0 && right<s.Length && s[left] == s[right])
-        {
-            left--;
-            right++;
+        return maxLengthStr.ToString();
         }
-        return right-left-1; // (right+1) - (left-1) - 1
+
     }
-}
