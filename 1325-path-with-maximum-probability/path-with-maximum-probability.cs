9a73805a9971381adc1d38970d3a3c9e2b4c7400
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2023-10-22 13:04:56 +0200

    Time: 269 ms (13.46%) | Memory: 54.5 MB (75.00%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cs
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cs
@@ -1,55 +1,54 @@
-        public class Solution
+    public class Solution
+    {
+        public double MaxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node)
         {
-            public double MaxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node)
+            // Bellman-Ford algorithm improved
+            var distances = new double[n];
+            distances[start_node] = 1;
+            var quue = new PriorityQueue<(int, double), double>(Comparer<double>.Create((double a, double b) => b.CompareTo(a)));
+
+            var graph = BuildGraph(edges, succProb);
+
+            // probability of start is 1
+            quue.Enqueue((start_node, 1), 1);
+
+            while (quue.Count > 0)
             {
-                // Bellman-Ford algorithm improved
-                var distances = new double[n];
-                distances[start_node] = 1;
-                var quue = new Queue<(int,double)>();
-
-                var graph = BuildGraph(edges, succProb);
-
-                // probability of start is 1
-                quue.Enqueue((start_node, 1));
-
-                while (quue.Count>0)
+                var pop = quue.Dequeue();
+                var neigbours = graph.ContainsKey(pop.Item1) ? graph[pop.Item1] : new List<(int, double)>();
+                foreach (var edge in neigbours)
                 {
-                    var pop = quue.Dequeue();
-
-                    var neigbours = graph.ContainsKey(pop.Item1)? graph[pop.Item1]: new List<(int, double)>();
-                    foreach (var edge in neigbours)
+                    var u = pop.Item1;
+                    var w = edge.Item1;
+                    var newDist = pop.Item2 * edge.Item2;
+                    if (newDist > distances[w])
                     {
-                        var u = pop.Item1;
-                        var w = edge.Item1;
-                        var newDist = pop.Item2 * edge.Item2;
-                        if (newDist > distances[w])
-                        {
-                            distances[w] = newDist;
-                            quue.Enqueue((w, newDist));
-                        }
+                        distances[w] = newDist;
+                        quue.Enqueue((w, newDist), newDist);
                     }
                 }
-
-                return distances[end_node];
             }
 
-            private Dictionary<int, List<(int, double)>> BuildGraph(int[][] edges, double[] succprobes)
-            {
-                var dict = new Dictionary<int, List<(int, double)>>();
-                for (int i = 0; i < edges.Length; i++)
-                {
-                    var edge = edges[i];
-                    var src = edge[0];
-                    var dst = edge[1];
-                    var prob = succprobes[i];
-                    if (!dict.ContainsKey(src))
-                        dict.Add(src, new List<(int, double)>());
-                    if(!dict.ContainsKey(dst))
-                        dict.Add(dst, new List<(int, double)>());
-                    dict[src].Add((dst, prob));
-                    dict[dst].Add((src, prob));
-                }
-
-                return dict;
-            }
+            return distances[end_node];
         }
+
+        private Dictionary<int, List<(int, double)>> BuildGraph(int[][] edges, double[] succprobes)
+        {
+            var dict = new Dictionary<int, List<(int, double)>>();
+            for (int i = 0; i < edges.Length; i++)
+            {
+                var edge = edges[i];
+                var src = edge[0];
+                var dst = edge[1];
+                var prob = succprobes[i];
+                if (!dict.ContainsKey(src))
+                    dict.Add(src, new List<(int, double)>());
+                if (!dict.ContainsKey(dst))
+                    dict.Add(dst, new List<(int, double)>());
+                dict[src].Add((dst, prob));
+                dict[dst].Add((src, prob));
+            }
+
+            return dict;
+        }
+    }
