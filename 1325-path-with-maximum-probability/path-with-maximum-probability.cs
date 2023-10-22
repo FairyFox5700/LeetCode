c1e39fe3c6b267338406cd144b53a1281f84f94c
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2023-10-22 12:22:22 +0200

    Time: 232 ms (82.69%) | Memory: 54.5 MB (80.77%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cs
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cs
@@ -1,34 +1,55 @@
-public class Solution
-{
-    public double MaxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node)
-    {
-        // Bellman-Ford algorithm
-        var distances = new double[n];
-        distances[start_node] = 1;
-
-        for (int i = 0; i < n - 1; i++)
+        public class Solution
         {
-            for (int j = 0; j < edges.Length; j++) // Fixed the loop index
+            public double MaxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node)
             {
-                var edge = edges[j];
-                var u = edge[0];
-                var v = edge[1];
-                var prob = succProb[j];
-                var tempDistanceU = distances[u] * prob;
-                var tempDistanceV = distances[v] * prob;
+                // Bellman-Ford algorithm improved
+                var distances = new double[n];
+                distances[start_node] = 1;
+                var quue = new Queue<(int,double)>();
 
-                if (distances[v] < tempDistanceU)
+                var graph = BuildGraph(edges, succProb);
+
+                // probability of start is 1
+                quue.Enqueue((start_node, 1));
+
+                while (quue.Count>0)
                 {
-                    distances[v] = tempDistanceU;
+                    var pop = quue.Dequeue();
+
+                    var neigbours = graph.ContainsKey(pop.Item1)? graph[pop.Item1]: new List<(int, double)>();
+                    foreach (var edge in neigbours)
+                    {
+                        var u = pop.Item1;
+                        var w = edge.Item1;
+                        var newDist = pop.Item2 * edge.Item2;
+                        if (newDist > distances[w])
+                        {
+                            distances[w] = newDist;
+                            quue.Enqueue((w, newDist));
+                        }
+                    }
                 }
 
-                if (distances[u] < tempDistanceV)
+                return distances[end_node];
+            }
+
+            private Dictionary<int, List<(int, double)>> BuildGraph(int[][] edges, double[] succprobes)
+            {
+                var dict = new Dictionary<int, List<(int, double)>>();
+                for (int i = 0; i < edges.Length; i++)
                 {
-                    distances[u] = tempDistanceV;
+                    var edge = edges[i];
+                    var src = edge[0];
+                    var dst = edge[1];
+                    var prob = succprobes[i];
+                    if (!dict.ContainsKey(src))
+                        dict.Add(src, new List<(int, double)>());
+                    if(!dict.ContainsKey(dst))
+                        dict.Add(dst, new List<(int, double)>());
+                    dict[src].Add((dst, prob));
+                    dict[dst].Add((src, prob));
                 }
+
+                return dict;
             }
         }
-
-        return distances[end_node];
-    }
-}
