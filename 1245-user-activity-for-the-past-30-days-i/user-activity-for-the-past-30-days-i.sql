586339b24fc55bd82c649a811a1673feeb2c5863
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2023-10-29 18:34:21 +0100

    Time: 1589 ms (96.87%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
 select  activity_date as day, count(DISTINCT user_id )as active_users
 from Activity
-WHERE   DATEDIFF(day,activity_date,'2019-07-27') < 30  AND DATEDIFF(day, activity_date,'2019-07-27')>=0
 group by  activity_date
+HAVING  DATEDIFF(day,activity_date,'2019-07-27') < 30  AND DATEDIFF(day, activity_date,'2019-07-27')>=0
