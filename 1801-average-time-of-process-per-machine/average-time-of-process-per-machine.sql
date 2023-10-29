2a9d0ddcb2aa288fe19865639b7a13d5f1e294fb
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2023-10-29 14:11:06 +0100

    Time: 741 ms (31.97%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,8 +1,4 @@
 /* Write your T-SQL query statement below 
-SELECT machine_id, processing_time 
-FROM Activity
-GROUP BY machine_id*/
-
 SELECT  machine_id, ROUND(SUM ( 
   CASE
   WHEN activity_type = 'start' THEN -1*timestamp
@@ -10,4 +6,13 @@ SELECT  machine_id, ROUND(SUM (
   END
 ) / COUNT(DISTINCT process_id), 3) as processing_time 
 FROM Activity
-GROUP BY machine_id
+GROUP BY machine_id*/
+
+SELECT a.machine_id, ROUND(AVG(b.timestamp - a.timestamp), 3) as  processing_time
+FROM Activity a
+JOIN Activity b
+ON a.machine_id = b.machine_id
+AND a.process_id = b.process_id
+AND a.activity_type =  'start'
+AND b.activity_type =  'end'
+GROUP BY a.machine_id
