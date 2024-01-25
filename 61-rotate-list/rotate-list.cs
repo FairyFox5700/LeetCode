f4610752fa2794f93e3db869d8fa0658821cc030
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-01-25 12:43:57 +0100

    Time: 63 ms (78.24%) | Memory: 41.8 MB (7.63%) - LeetSync

---
+++ 61-rotate-list/rotate-list.cs
@@ -0,0 +1,33 @@
+    public class Solution
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            if(head == null)
+                return null;
+            if(k==0)
+                return head;
+            var lastEl = head;
+            // [1,2]
+            var count = 0;
+            while (lastEl.next!= null)
+            {
+                lastEl = lastEl.next;
+                count++;
+            }
+           
+            lastEl.next = head; // connected to cycle
+
+            var current = head;
+            var skip = count-(k%(count+1));
+             Console.WriteLine(skip);
+            while ((skip)>0)
+            {
+                current = current.next;
+                skip--;
+            }
+            var newHead = current?.next;
+            current.next = null; // mark the end of the list
+
+            return newHead;
+        }
+    }
