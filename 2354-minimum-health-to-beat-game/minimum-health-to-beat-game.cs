09271be7840b098da6e71ac31883bfa7610eb436
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-02-09 20:21:32 +0100

    Time: 247 ms (75.00%) | Memory: 69.1 MB (25.00%) - LeetSync

--- 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.cs
+++ 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.cs
@@ -3,17 +3,34 @@ public class Solution
     public long MinimumHealth(int[] damage, int armor)
     {
         var max = 0L;
+        var maxIndex = -1;
         var sum = 0L;
         for (int i = 0; i < damage.Length; i++)
         {
+      
                 if(max< damage[i])
                 {
                     max = damage[i];
+                    maxIndex = i;
                 }
+                
+          
+
             sum += damage[i];
         }
 
-            return  sum - Math.Min(armor, max) +1;
+        if (maxIndex != -1 && armor > 0)
+        {
+            if(armor > max)
+            {
+                 return sum - max +1;
+            }
+            var helpFromArmor = max - armor;
+            return (sum - armor +1);
         }
 
+        return sum + 1;
+
+
     }
+}
