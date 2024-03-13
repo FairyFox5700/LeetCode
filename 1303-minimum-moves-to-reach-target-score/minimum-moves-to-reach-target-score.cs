aeb757db5b644a16d9b70dded21afcf13c9fe1f0
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-03-13 21:14:39 +0200

    Time: 21 ms (80.00%) | Memory: 26.8 MB (66.67%) - LeetSync

--- 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cs
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cs
@@ -1,20 +1,54 @@
-class Solution {
+public class Solution {
     public int MinMoves(int target, int maxDoubles) {
-        int c=0;
-        while(target>1 && maxDoubles>0)
-        {
-            if(target%2==0)
-            {
-                maxDoubles-=1;
-                target/=2;
-                c+=1;
-            }
-            else
-            {
-                target-=1;
-                c+=1;
-            }
-        }
-        return c+(target-1);
+        if(target == 1)
+            return 0;
+        if(maxDoubles == 0)
+            return target -1; // only one way to reach target, to descrease each time
+        if(target%2==0 && maxDoubles >0)
+         return 1+ MinMoves(target/2, maxDoubles-1);
+        return 1+ MinMoves(target-1, maxDoubles);
     }
 }
+
+/* BFS
+    public class Solution
+    {
+        public int MinMoves(int target, int maxDoubles)
+        {
+            var queue = new Queue<(int, int)>();
+            queue.Enqueue((target, 0));
+            var visited = new HashSet<int>();
+        
+            while (queue.Count > 0)
+            {
+         
+                    var (el, steps) = queue.Dequeue();
+                    Console.WriteLine(el);
+                    if (el == 1)
+                    {
+                        return steps;
+                    }
+
+                    if (maxDoubles > 0 && el%2 == 0)
+                    {
+                        if (!visited.Contains(el/2))
+                        {
+                            queue.Enqueue((el/2, steps+1));
+                            visited.Add(el/2);
+                            maxDoubles--;
+                        }
+                      
+                    }
+                
+                    if (!visited.Contains(el - 1))
+                    {
+                        queue.Enqueue((el - 1, steps +1));
+                        visited.Add(el - 1);
+                    }
+            }
+
+            return -1;
+        }
+    }
+
+    */
