c16aff5468b2a6d457371816d0858c9149475422
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-03-15 17:13:56 +0200

    Time: 134 ms (14.59%) | Memory: 47 MB (7.30%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cs
+++ 45-jump-game-ii/jump-game-ii.cs
@@ -1,36 +1,32 @@
-public class Solution {
-          public int Jump(int[] nums)
+    public class Solution
+    {
+        // 2 3 1 1 4
+        public int Jump(int[] nums)
         {
-            if(nums.Length <= 1)
-            {
-                return 0; // no jump requred
-            }
-            // max window we can reac from current location
-            var left = 0;
-            var right = 0;
-            var maxReachedIndex = 0;
-            var jumps = 0;
+            var maxReach = 0;
+            var steps = 0;
+            var min = int.MaxValue;
+            var maxEnd = 0;
+            var target = nums.Length - 1;
+            if(nums.Length == 1) return 0;
 
             for (int i = 0; i < nums.Length; i++)
             {
-                // we try for each index in the reachable window
-                for (int j = left; j <= right; j++)
+                maxReach = Math.Max(maxReach, i + nums[i]);
+                Console.WriteLine(maxReach);
+
+                if(target<=maxReach)
                 {
-                    // max distance we can reach
-                    // if we are on poistion 1, then the max index we can reach is 1 + nums[1]
-                    // ex. arr = [2,3,1,1,4], reached = 1(index) + 3(nums[1]) = 4
-                    maxReachedIndex = Math.Max(maxReachedIndex, nums[j] + j);
+                    return steps +1;
                 }
-                Console.WriteLine($"left: {left}, right: {right}, maxReachedIndex: {maxReachedIndex}");
-                left = right; // current index
-                right = maxReachedIndex; // max index we can reach from current index
-                jumps++;
-                if(maxReachedIndex >=  nums.Length -1)
+
+                if (i == maxEnd)
                 {
-                    return jumps;
+                    steps++;
+                    maxEnd = maxReach;
                 }
             }
 
-            return jumps;
+            return steps;
         }
-}
+    }
