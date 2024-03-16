d471a7cebebdc9dfd14468cbefe2e1cad9815a3e
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-03-16 09:29:30 +0200

    Time: 18 ms (79.37%) | Memory: 26.6 MB (92.82%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cs
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cs
@@ -1,25 +1,37 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
 public class Solution : GuessGame {
     public int GuessNumber(int n) {
-        int left = 1;
-        int right = n;
-        while (left <= right) {
-            int mid1 = left + (right - left) / 3;
-            int mid2 = right - (right - left) / 3;
-            int res1 = guess(mid1);
-            int res2 = guess(mid2);
-            
-            if (res1 == 0) return mid1;
-            if (res2 == 0) return mid2;
-            
-            if (res1 < 0) {
-                right = mid1 - 1; // The target is less than mid1
-            } else if (res2 > 0) {
-                left = mid2 + 1; // The target is greater than mid2
-            } else {
-                left = mid1 + 1; // The target is between mid1 and mid2
-                right = mid2 - 1;
+        var left = 1;
+        var right = n;
+        while(left<=right)
+        {
+            var mid1 = left + (right - left) / 3;
+            var mid2 = right - (right - left)/3;
+            var number1 = guess(mid1);
+            var number2 = guess(mid2);
+            if(number1 == 0)
+                return mid1;
+            if(number2 == 0)
+                return mid2;
+            else if( number1 < 0)
+                right = mid1 -1;
+            else if(number2 >0)
+                left = mid2+1;
+            else
+            {
+                right = mid2 -1;
+                left = mid1+1;
             }
         }
-        return -1; // This should theoretically never be reached if the API behaves as expected
+
+        return -1;
     }
 }
