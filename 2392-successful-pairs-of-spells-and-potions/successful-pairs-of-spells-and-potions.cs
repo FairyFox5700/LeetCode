2d72afcd5f1689190835f8a0d5e30e81d96d52cb
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-03-17 14:58:44 +0200

    Time: 492 ms (6.78%) | Memory: 81.7 MB (78.39%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cs
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cs
@@ -1,25 +1,25 @@
     public class Solution
     {
-    private int GetLowerBoundIndex(int[] spells, int[] sortedPotions, long success, int spellsIndex)
-    {
-        var right = sortedPotions.Length - 1;
-        var left = 0;
-        while (left <= right)
+        private int GetLowerBoundIndex(int[] spells, int[] sortedPotions, long success, int spellsIndex)
         {
-            var mid = left + (right - left) / 2;
-            var data = (long)spells[spellsIndex] * sortedPotions[mid];
-            if (data < success)
+            var right = sortedPotions.Length-1;
+            var left = 0;
+            while (left <= right)
             {
-                left = mid + 1;
+                var mid = left + (right - left) / 2;
+                long data = (long)spells[spellsIndex] * sortedPotions[mid];
+                if (data < success)
+                {
+                    left = mid + 1;
+                }
+                else if (data >= success)
+                {
+                    right = mid - 1;
+                }
             }
-            else
-            {
-                right = mid - 1;
-            }
-        }
 
-        return left;
-    }
+            return left;
+        }
 
     public int[] SuccessfulPairs(int[] spells, int[] potions, long success)
     {
