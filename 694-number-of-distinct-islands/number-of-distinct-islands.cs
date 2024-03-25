cc810063e8c1ab09c87548b7617bb147b64cf6eb
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-03-25 16:33:11 +0200

    Time: 124 ms (35.90%) | Memory: 52.7 MB (69.23%) - LeetSync

---
+++ 694-number-of-distinct-islands/number-of-distinct-islands.cs
@@ -0,0 +1,51 @@
+    public class Solution
+    {
+        public int NumDistinctIslands(int[][] grid)
+        {
+            var visited = new HashSet<(int, int)>();
+
+            var hashSet = new HashSet<string>();
+
+            for (int r = 0; r < grid.Length; r++)
+            {
+                for (int c = 0; c < grid[0].Length; c++)
+                {
+                    if (grid[r][c] == 1 && !visited.Contains((r, c)))
+                    {
+
+                        var strBuilder = new StringBuilder();
+                        DFS(grid, r, c, visited, ' ', strBuilder);
+                        Console.WriteLine(strBuilder.ToString());
+                        if(!hashSet.Contains(strBuilder.ToString()))
+                        {
+                            hashSet.Add(strBuilder.ToString());
+                        }
+                    }
+                }
+            }
+
+            return hashSet.Count;
+        }
+
+        private void DFS(int[][] grid, int r, int c, HashSet<(int, int)> visited, char direction, StringBuilder stringBuilder)
+        {
+            // to validate
+            if (r < 0 || r >= grid.Length || c < 0 || c >= grid[0].Length)
+            {
+                return;
+            }
+
+            if (visited.Contains((r, c)) || grid[r][c]!=1)
+            {
+                return;
+            }
+
+            visited.Add((r, c));
+            stringBuilder.Append(direction);
+            DFS(grid, r+1, c, visited, 'D', stringBuilder);
+            DFS(grid, r-1, c, visited, 'U', stringBuilder);
+            DFS(grid, r, c+1, visited, 'R', stringBuilder);
+            DFS(grid, r, c-1, visited, 'L', stringBuilder);
+            stringBuilder.Append(' ');
+        }
+    }
