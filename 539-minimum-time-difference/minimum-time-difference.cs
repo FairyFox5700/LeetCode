08ddef980071cc5a8d9a272e4a53d3d217427a5e
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-04-05 12:36:13 +0200

    Time: 98 ms (13.89%) | Memory: 47.7 MB (11.11%) - LeetSync

---
+++ 539-minimum-time-difference/minimum-time-difference.cs
@@ -0,0 +1,30 @@
+    public class Solution
+    {
+        public int FindMinDifference(IList<string> timePoints)
+        {
+            var listTime = new List<int>();
+            for (int i = 0; i < timePoints.Count; i++)
+            {
+                var point = timePoints[i].Split(":");
+                var p1 = int.Parse(point[0]) ;
+                var p2 = int.Parse(point[1]) ;
+                var totalMinutes = p1 * 60 + p2;
+                listTime.Add(totalMinutes);
+            }
+
+            listTime.Sort();
+            Console.WriteLine(string.Join(",", listTime));
+            var min = int.MaxValue;
+            for (int i = 1; i < listTime.Count; i++)
+            {
+                var diff = Math.Abs(listTime[i]- listTime[i-1]);
+                Console.WriteLine(diff);
+                min = Math.Min(diff , min);
+            }
+
+            // calculate circulal nature of timePoints
+            var circularDiff =  Math.Abs(listTime[0]+ 24 *60 - listTime[listTime.Count-1]);
+            min = Math.Min(circularDiff  , min);
+            return min;
+        }
+    }
