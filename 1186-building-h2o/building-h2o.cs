7b55037a87e569f2dc5047a9efb1a1b22e228a8a
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-04-08 12:35:23 +0200

    Time: 61 ms (81.44%) | Memory: 43.6 MB (51.55%) - LeetSync

--- 1186-building-h2o/building-h2o.cs
+++ 1186-building-h2o/building-h2o.cs
@@ -36,3 +36,41 @@ public class H2O
         }
     }
 }
+
+/* other option
+using System.Threading;
+
+public class H2O
+    {
+        private readonly SemaphoreSlim semaphoreH = new SemaphoreSlim(1);
+        private readonly SemaphoreSlim semaphoreO = new SemaphoreSlim(1);
+        private int _numberH;
+
+        public void Hydrogen(Action releaseHydrogen)
+        {
+            semaphoreH.Wait();
+
+            // releaseHydrogen() outputs "H". Do not change or remove this line.
+            releaseHydrogen();
+
+            if (Interlocked.Increment(ref _numberH) % 2 == 0)
+            {
+                semaphoreO.Release();
+            }
+            else
+            {
+                semaphoreH.Release();
+            }
+        }
+
+        public void Oxygen(Action releaseOxygen)
+        {
+            semaphoreO.Wait();
+            // releaseOxygen() outputs "O". Do not change or remove this line.
+            releaseOxygen();
+
+            semaphoreH.Release();
+        }
+    }
+
+*/
