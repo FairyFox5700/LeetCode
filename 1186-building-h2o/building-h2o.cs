cc1491ae1fd49395e94f53ecd1c0a927a16c6121
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-04-08 12:34:32 +0200

    Time: 65 ms (70.10%) | Memory: 43.3 MB (84.54%) - LeetSync

--- 1186-building-h2o/building-h2o.cs
+++ 1186-building-h2o/building-h2o.cs
@@ -1,34 +1,38 @@
+using System;
 using System.Threading;
 
-public class H2O {
-    private readonly SemaphoreSlim semaphoreH = new SemaphoreSlim(2);
-    private readonly SemaphoreSlim semaphoreO = new SemaphoreSlim(1);
-
-    private readonly object _locker = new object();
-    private int _numberH;
+public class H2O
+{
+    private SemaphoreSlim hydrogenSemaphore = new SemaphoreSlim(2, 2); // Allows up to 2 hydrogens to proceed
+    private SemaphoreSlim oxygenSemaphore = new SemaphoreSlim(0, 1); // Initially blocks oxygen
+    private int hydrogenCount = 0;
+    private object lockObject = new object(); // For atomic operations
 
     public void Hydrogen(Action releaseHydrogen)
     {
-        semaphoreH.Wait();
-
-        lock (_locker)
+        hydrogenSemaphore.Wait(); // Wait for the semaphore to be available
+ 
+        lock (lockObject)
         {
-            // releaseHydrogen() outputs "H". Do not change or remove this line.
-            releaseHydrogen();
+            releaseHydrogen(); // Release hydrogen
 
-            if (++_numberH % 2 == 0)
+            hydrogenCount++;
+            if (hydrogenCount == 2) // If this is the second hydrogen, allow oxygen to proceed
             {
-                semaphoreO.Release();
+                oxygenSemaphore.Release();
             }
         }
     }
 
     public void Oxygen(Action releaseOxygen)
     {
-        semaphoreO.Wait();
-        // releaseOxygen() outputs "O". Do not change or remove this line.
-        releaseOxygen();
+        oxygenSemaphore.Wait(); // Wait for two hydrogens to be released
 
-        semaphoreH.Release(2);
+        lock (lockObject)
+        {
+            releaseOxygen(); // Release oxygen
+            hydrogenCount = 0; // Reset hydrogen count for the next molecule
+            hydrogenSemaphore.Release(2); // Allow next two hydrogens to proceed
+        }
     }
 }
