f429b83c25d0232f75fe50b08baaf1a53cd266ba
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-04-28 12:22:47 +0200

    Time: 70 ms (68.36%) | Memory: 44.1 MB (72.00%) - LeetSync

--- 179-largest-number/largest-number.cs
+++ 179-largest-number/largest-number.cs
@@ -1,21 +1,34 @@
-    public class Solution
-    {
-        public string LargestNumber(int[] nums)
-        {
-            var strings = nums.Select(x => x.ToString()).ToList();
-            strings.Sort((a, b) =>
-            {
-                var num1 = a + b;
-                var num2 = b + a;
-                return num2.CompareTo(num1);
-            });
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-
-            if (strings[0] == "0")
-            {
-                return "0";
-            }
-
-            return string.Join("", strings);
+public class Solution {
+    private class LargerNumberComparer : IComparer<string> {
+        public int Compare(string a, string b) {
+            // Compare two strings by their potential concatenated results without creating new strings
+            return (b + a).CompareTo(a + b);
         }
     }
+
+    public string LargestNumber(int[] nums) {
+        // Early check for a scenario where all numbers could be zero
+        if (nums.All(n => n == 0)) {
+            return "0";
+        }
+
+        // Convert all numbers to strings
+        var strings = nums.Select(x => x.ToString()).ToList();
+
+        // Sort the strings using a custom comparer
+        strings.Sort(new LargerNumberComparer());
+
+        // After sorting, if the first item is "0", return "0" because all others must be zero too
+        if (strings[0] == "0") {
+            return "0";
+        }
+
+        // Join all strings to form the largest number
+        return string.Join("", strings);
+    }
+}
+
