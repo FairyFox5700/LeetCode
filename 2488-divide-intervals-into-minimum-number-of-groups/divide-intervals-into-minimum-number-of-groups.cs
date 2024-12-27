19e659e7f74342a9c92f9db37f2fc04226f42671
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-12-27 11:17:11 +0100

    Time: 149 ms (35.42%) | Memory: 97.2 MB (41.67%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cs
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cs
@@ -8,12 +8,12 @@
                 {
                     var range = intervals[i];
                     events.Add((range[0], 1));
-                    events.Add((range[1], -1));
+                    events.Add((range[1]+1, -1));
                 }
 
                 // [1,1]], 1, -1 is cancelling , 1,+1 , but we have one active ( first +1 , then -1)
                 events = events.OrderBy(e => e.Item1)
-                    .ThenByDescending(e => e.Item2).ToList();
+                    .ThenBy(e => e.Item2).ToList();
 
                 var maxActiveOverlaps = 0;
                 var currentActiveOverlaps = 0;
@@ -21,6 +21,7 @@
                 for (int i = 0; i < events.Count(); i++)
                 {
                     var current = events[i];
+               
                     if (current.Item2 > 0)
                     {
                         currentActiveOverlaps++;
