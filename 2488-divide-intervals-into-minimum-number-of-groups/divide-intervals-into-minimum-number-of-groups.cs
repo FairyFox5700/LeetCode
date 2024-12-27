9d32ad5268a8aac0e31d6a9a3c11b1bf04765a68
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2024-12-27 11:04:04 +0100

    Time: 179 ms (16.67%) | Memory: 96.7 MB (43.75%) - LeetSync

---
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cs
@@ -0,0 +1,38 @@
+        public class Solution
+        {
+            public int MinGroups(int[][] intervals)
+            {
+                var events = new List<(int, int)>();
+
+                for (int i = 0; i < intervals.Count(); i++)
+                {
+                    var range = intervals[i];
+                    events.Add((range[0], 1));
+                    events.Add((range[1], -1));
+                }
+
+                events = events.OrderBy(e => e.Item1)
+                    .ThenByDescending(e => e.Item2).ToList();
+
+                var maxActiveOverlaps = 0;
+                var currentActiveOverlaps = 0;
+
+                for (int i = 0; i < events.Count(); i++)
+                {
+                    var current = events[i];
+                    if (current.Item2 > 0)
+                    {
+                        currentActiveOverlaps++;
+                    }
+                    if(current.Item2 < 0)
+                    {
+                        currentActiveOverlaps--;
+                    }
+
+                    maxActiveOverlaps = Math.Max(maxActiveOverlaps, currentActiveOverlaps);
+                }
+
+                return maxActiveOverlaps;
+            }
+          
+        }
