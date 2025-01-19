8af0efbe10e703e2289a6bd15e77d878140914ff
Tyshchenko Iryna <46414904+FairyFox5700@users.noreply.github.com>
2025-01-19 17:10:06 +0200

    Time: 13 ms (33.33%) | Memory: 76.3 MB (33.33%) - LeetSync

--- 2084-maximum-number-of-weeks-for-which-you-can-work/maximum-number-of-weeks-for-which-you-can-work.cs
+++ 2084-maximum-number-of-weeks-for-which-you-can-work/maximum-number-of-weeks-for-which-you-can-work.cs
@@ -1,6 +1,10 @@
 public class Solution {
     public long NumberOfWeeks(int[] milestones) {
         
+
+        /*
+        If the sum of the other projects (otherSum) is greater than or equal to the largest project’s milestones, then you can continue alternating without constraint, and the maximum weeks will be the sum of all milestones (totalSum).
+If the largest project has more milestones than the sum of the other projects, you can only work for 2 * otherSum + 1 weeks because, once the smaller projects run out, you won't have anything left to alternate with.*/
     long max = milestones.Max();
     long totalSum = milestones.Sum(e=>(long)e);
     var otherSum = totalSum - max;
